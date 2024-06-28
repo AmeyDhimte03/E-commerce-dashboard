d5761d95116e3fc574ee64eb99cc58a71ab1d92a
Amey Dhimte <ameydhimte03@gmail.com>
2024-06-28 23:36:42 +0530

    1st commit

---
+++ backend/db/config.js
@@ -0,0 +1,9 @@
+const mongoose= require('mongoose');
+// mongoose.connect("mongodb://localhost:27017/e-commerce")
+dbName = "e-commerce";
+
+MONGO_URI="mongodb+srv://ameydhimtecoding:mongodbdatabasepassword@cluster0.rgln5qn.mongodb.net/"+dbName;
+
+mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+});
